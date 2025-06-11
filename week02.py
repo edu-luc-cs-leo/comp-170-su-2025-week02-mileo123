74ca0de6487415f4a60e27370cc94cb7c5bdbfe1
mileo123 <mgray12@luc.edu>
2025-06-11 16:23:28 -0500

    Update week02.py

--- week02.py
+++ week02.py
@@ -1,5 +1,6 @@
 // Miles Gray
 def greet(name):
     #def defines the fucntion which is greet
-return f"Hello {name}. How are you?"
+    return f"Hello {name}. How are you?"
 
+print(greet("Miles"))
