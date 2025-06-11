ace2adb26c759f1bb6328ce69bed726b6842e6d7
mileo123 <mgray12@luc.edu>
2025-06-11 16:30:04 -0500

    Update week02.py

--- week02.py
+++ week02.py
@@ -4,3 +4,14 @@ def greet(name):
     return f"Hello {name}. How are you?"
 
 print(greet("Miles"))
+
+#friends
+my_friends = ["Carl", "Vince", "Paul"]
+
+#the list
+
+def greet_friends(friend_list):
+   for friend in friend_list:
+       print(greet(friend))
+
+greet_friends(my_friends)
